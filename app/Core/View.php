ea82f93d19effda526a906a5bde15a37fde6157a
Jorge Augusto Mabuie <jorgemaabuie@gmail.com>
2026-03-30 07:31:03 -0700

    Fix view rendering path override in layout includes

--- app/Core/View.php
+++ app/Core/View.php
@@ -9,16 +9,16 @@ class View
     public static function render(string $template, array $data = []): void
     {
         extract($data, EXTR_SKIP);
-        $path = __DIR__ . '/../Views/' . $template . '.php';
+        $templatePath = __DIR__ . '/../Views/' . $template . '.php';
 
-        if (!file_exists($path)) {
+        if (!file_exists($templatePath)) {
             http_response_code(404);
             echo 'Template não encontrado.';
             return;
         }
 
         require __DIR__ . '/../Views/layouts/header.php';
-        require $path;
+        require __DIR__ . '/../Views/' . $template . '.php';
         require __DIR__ . '/../Views/layouts/footer.php';
     }
 }
